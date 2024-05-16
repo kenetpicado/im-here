355de1be603eb23655bf3cf9b7f503aec93054fa
Kenet Picado <kenetpicado1@gmail.com>
2024-05-16 10:39:44 -0600

    Update pre-deploy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 git add .
-git commit -m "update"
+git commit -m "Update pre-deploy"
 git push
 
 npm run build
@@ -15,8 +15,8 @@ cd dist
 
 git init
 git add .
-git commit -m 'deploy'
+git commit -m 'Deploy'
 
-git push -f git@github.com:kenetpicado/tracker.git master:gh-pages
+git push -f git@github.com:kenetpicado/im-here.git master:gh-pages
 
 cd -
